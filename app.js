3d0637855805b0f90f4d600f146633d562799245
leologo11 <mendozaleologo@gmail.com>
2026-03-17 18:50:10 -0300

    fix dotenv production

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 // app.js v4
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
